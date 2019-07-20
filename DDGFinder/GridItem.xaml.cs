e92009457cebb4edd7b8805f5bea4b67ee492f69
newage99 <rooger99@gmail.com>
2019-07-20 15:04:51 +0200

    First commit.

---
+++ DDGFinder/GridItem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DDGFinder
+{
+    public partial class GridItem : UserControl
+    {
+        public GridItem()
+        {
+            InitializeComponent();
+        }
+
+        private string idValue = "";
+
+        public string Id
+        {
+            get { return idValue; }
+            set { idValue = value; }
+        }
+
+        private string stateOrResultValue = "";
+
+        public string StateOrResult
+        {
+            get { return stateOrResultValue; }
+            set { stateOrResultValue = value; }
+        }
+    }
+}
---
+++ DDGFinder/GridItemClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDGFinder
+{
+    public class GridItemClass
+    {
+        private string idValue = "";
+
+        public string Id
+        {
+            get { return idValue; }
+        }
+
+        private int degreeValue = 0;
+        private int diameterValue = 0;
+
+        public string StateOrResult
+        {
+            get { return degreeValue.ToString() + "  " + diameterValue.ToString(); }
+        }
+
+        public void setId(string id)
+        {
+            idValue = id;
+        }
+        public void setDegree(int degree)
+        {
+            degreeValue = degree;
+        }
+        public void setDiameter(int diameter)
+        {
+            diameterValue = diameter;
+        }
+    }
+}
