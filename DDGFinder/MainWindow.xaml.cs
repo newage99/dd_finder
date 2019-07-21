3bb63b75db691e692e530f4c00c224701800d2e7
newage99 <rooger99@gmail.com>
2019-07-21 02:30:43 +0200

    Implemented method to get a random id, but is too slow.

--- DDGFinder/MainWindow.xaml.cs
+++ DDGFinder/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DDGFinder
 {
@@ -19,6 +22,64 @@ namespace DDGFinder
             new BindableTwoDArray<string>(10, 10);
         private BindableTwoDArray<string> stateOrResultsValues =
             new BindableTwoDArray<string>(10, 10);
+        private int numberOfNodesValue = 1;
+        private int minLengthValue = 8;
+        private int maxLengthValue = 20;
+        private bool[,,,] topologies;
+        private static string characters = "xyn+-*/%()12";
+        private static char[] numbers_array = new char[] { 'x', 'y', 'n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static char[] xyn_array = new char[] { 'x', 'y', 'n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(' };
+        private static char[] plus_minus_array = new char[] { '+', '-', '*', '/', '%', ')' };
+        private static char[] operators_array = new char[] { '+', '*', '/', '%', ')' };
+        private static Dictionary<char, char[]> forbidden_right_chars = new Dictionary<char, char[]>()
+        {
+            { 'x', xyn_array },
+            { 'y', xyn_array },
+            { 'n', xyn_array },
+            { '+', plus_minus_array },
+            { '-', plus_minus_array },
+            { '*', operators_array },
+            { '/', operators_array },
+            { '%', operators_array },
+            { '(', new char[] { '+', '*', '/', '%', ')' } },
+            { ')', numbers_array },
+            { '0', numbers_array },
+            { '1', numbers_array },
+            { '2', numbers_array },
+            { '3', numbers_array },
+            { '4', numbers_array },
+            { '5', numbers_array },
+            { '6', numbers_array },
+            { '7', numbers_array },
+            { '8', numbers_array },
+            { '9', numbers_array }
+        };
+        private static char[] array_left = new char[] { 'x', 'y', 'n', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static char[] operators_and_close_parenthesis_array_left = new char[] { '+', '-', '*', '/', '%', '(' };
+        private static Dictionary<char, char[]> forbidden_left_chars = new Dictionary<char, char[]>()
+        {
+            { 'x', array_left },
+            { 'y', array_left },
+            { 'n', array_left },
+            { '+', operators_and_close_parenthesis_array_left },
+            { '-', new char[] { '+', '-' } },
+            { '*', operators_and_close_parenthesis_array_left },
+            { '/', operators_and_close_parenthesis_array_left },
+            { '%', operators_and_close_parenthesis_array_left },
+            { '(', array_left },
+            { ')', operators_and_close_parenthesis_array_left },
+            { '0', array_left },
+            { '1', array_left },
+            { '2', array_left },
+            { '3', array_left },
+            { '4', array_left },
+            { '5', array_left },
+            { '6', array_left },
+            { '7', array_left },
+            { '8', array_left },
+            { '9', array_left }
+        };
+
         public BindableTwoDArray<string> Ids
         {
             get { return idsValues; }
@@ -35,13 +96,42 @@ namespace DDGFinder
         {
             get { return appState == AppState.DOING_ITERATIONS && !requestedToStopIterating; }
         }
+        public bool StateNotLoadingOrDoingIterations
+        {
+            get { return appState != AppState.LOADING_TOPOLOGIES || appState != AppState.DOING_ITERATIONS; }
+        }
+        public string NumberOfNodes
+        {
+            get { return numberOfNodesValue.ToString(); }
+            set
+            {
+                if (!int.TryParse(value, out numberOfNodesValue))
+                    numberOfNodesValue = 0;
+            }
+        }
+        public string MinLength
+        {
+            get { return minLengthValue.ToString(); }
+            set
+            {
+                if (!int.TryParse(value, out minLengthValue))
+                    minLengthValue = 0;
+            }
+        }
+        public string MaxLength
+        {
+            get { return maxLengthValue.ToString(); }
+            set
+            {
+                if (!int.TryParse(value, out maxLengthValue))
+                    maxLengthValue = 0;
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
-            idsValues[0, 0] = "asdasd";
-            idsValues[0, 1] = "asdasd";
         }
 
         private static readonly Regex _regex = new Regex("[^0-9.-]+");
@@ -57,7 +147,172 @@ namespace DDGFinder
 
         private void Init_Click(object sender, RoutedEventArgs e)
         {
+            topologies = new bool[10, 10, numberOfNodesValue, numberOfNodesValue];
+            idsValues[0, 0] = getRandomId();
+        }
 
+        private int validateId(string id)
+        {
+            int length = id.Length;
+            char start = id[0];
+            if (start == '+' || start == '*' || start == '/' || start == '%' || start == ')')
+                return length - 1;
+            char prev = ' ';
+            char actual = id[0];
+            char next;
+            for(int i = 1; i < length; i++)
+            {
+                if (i < id.Length)
+                    next = id[i];
+                else next = ' ';
+                if (forbidden_left_chars[actual].Contains(prev) || forbidden_right_chars[actual].Contains(next))
+                    return i;
+                prev = actual;
+                actual = next;
+            }
+            char last = id.Last();
+            if (last == '+' || last == '-' || last == '*' || last == '/' || last == '%' || last == '(')
+                return length - 1;
+            if (!id.Contains('x') || !id.Contains('y'))
+                return length - 1;
+            if ((id.Contains('y') && !id.Contains('x')) || (id.Contains('z') && (!id.Contains('x') || !id.Contains('y'))))
+                return length - 1;
+            bool contains_variables = false;
+            bool wrong_parenthesis = false;
+            int parenthesis_counter = 0;
+            bool parenthesis_with_one_space_only = false;
+            int vars_since_last_opening_parenthesis = 0;
+            last = ' ';
+            bool double_parenthesis_starts = false;
+            bool there_is_a_double_parenthesis = false;
+            for(int i = 0; i < length; i++)
+            {
+                char c = id[i];
+                if ("xyzn".Contains(c))
+                    contains_variables = true;
+                if (c == '(')
+                {
+                    parenthesis_counter += 1;
+                    vars_since_last_opening_parenthesis = 0;
+                    if (last == '(')
+                        double_parenthesis_starts = true;
+                }
+                else
+                {
+                    if (c == ')')
+                    {
+                        if (last == ')' && double_parenthesis_starts)
+                        {
+                            there_is_a_double_parenthesis = true;
+                            break;
+                        }
+                        parenthesis_counter -= 1;
+                        if (vars_since_last_opening_parenthesis < 2)
+                        {
+                            parenthesis_with_one_space_only = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (last == ')')
+                        {
+                            double_parenthesis_starts = false;
+                            break;
+                        }
+                        vars_since_last_opening_parenthesis += 1;
+                    }
+                }
+                if (parenthesis_counter < 0)
+                {
+                    wrong_parenthesis = true;
+                    break;
+                }
+                last = c;
+            }
+            if (!contains_variables || wrong_parenthesis || parenthesis_counter != 0
+                || parenthesis_with_one_space_only || there_is_a_double_parenthesis)
+                return length - 1;
+            if (id.StartsWith("(") && id.EndsWith(")"))
+                return length - 1;
+            return 0;
+        }
+
+        private bool nextId(ref string id, int pointer)
+        {
+            int length = id.Length;
+            int model_pos = -1;
+            if (pointer < length - 1)
+            {
+                if (pointer > 0)
+                {
+                    string aux = "";
+                    for (int i = pointer + 1; i < length; i++)
+                    {
+                        aux += characters[0];
+                    }
+                    id = id.Substring(0, pointer + 1) + aux;
+                }
+                else pointer = length - 1;
+            }
+            else pointer = length - 1;
+            while (pointer >= 0)
+            {
+                char char_to_look_at = id[pointer];
+                int characters_length = characters.Length;
+                for(int i = 0; i < characters_length; i++)
+                {
+                    if (characters[i] == char_to_look_at)
+                    {
+                        model_pos = i;
+                        break;
+                    }
+                }
+                if (model_pos < characters_length - 1)
+                {
+                    id = id.Substring(0, pointer) + characters[model_pos + 1];
+                    for (int j = id.Length; j < length; j++)
+                    {
+                        id += characters[0];
+                    }
+                    return true;
+                }
+                else pointer -= 1;
+            }
+            return false;
+        }
+
+        private string getRandomId()
+        {
+            string result = "";
+            bool random_string_not_done = true;
+            while(random_string_not_done)
+            {
+                Random random = new Random();
+                int length = random.Next(minLengthValue, maxLengthValue);
+                char aux = characters[random.Next(0, characters.Length - 1)];
+                while (aux == '+' || aux == '*' || aux == '/' || aux == '%' || aux == ')')
+                    aux = characters[random.Next(0, characters.Length - 1)];
+                result += aux;
+                for (int i = 1; i < length; i++)
+                {
+                    result += characters[random.Next(0, characters.Length - 1)];
+                }
+                int pointer = validateId(result);
+                bool not_arrived_to_end = true;
+                while (pointer != 0)
+                {
+                    if (!nextId(ref result, pointer))
+                    {
+                        not_arrived_to_end = false;
+                        break;
+                    }
+                    pointer = validateId(result);
+                }
+                if (not_arrived_to_end)
+                    random_string_not_done = false;
+            }
+            return result;
         }
     }
 }
